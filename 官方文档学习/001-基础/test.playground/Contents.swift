68dfc78f9353bb281e0b98958f7d8aafe7bf92f0
解向前 <583986922@qq.com>
2018-09-10 10:45:49 +0800

    官方文档-基础

---
+++ 官方文档学习/001-基础/test.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a: String? = "sss"
+a = nil
+
+var b: String = "test"
+//b = nil    因为nil代表缺省,没有值,所以而变量b为字符串,不能赋值为 nil
+
+var c: Int?
+c = nil
+
+/*
+    只有可选值可以赋值为nil
+    所有类型 都可以赋值为nil
+ */
